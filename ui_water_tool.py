c9bf2ce00aca6a748297fca739f9b82f4d921d94
alekssworm <alekseyteleshko@gmail.com>
2025-06-06 01:55:13 +0200

    30 ai panel test

--- ui_water_tool.py
+++ ui_water_tool.py
@@ -15,15 +15,14 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QGridLayout,
-    QGroupBox, QLabel, QPushButton, QSizePolicy,
-    QSplitter, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QLabel,
+    QPushButton, QSizePolicy, QSplitter, QWidget)
 
 class Ui_water_tool(object):
     def setupUi(self, water_tool):
         if not water_tool.objectName():
             water_tool.setObjectName(u"water_tool")
-        water_tool.resize(278, 438)
+        water_tool.resize(278, 216)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
         sizePolicy.setHorizontalStretch(28)
         sizePolicy.setVerticalStretch(0)
@@ -43,18 +42,6 @@ class Ui_water_tool(object):
         self.frame_7.setFrameShadow(QFrame.Shadow.Raised)
         self.gridLayout_3 = QGridLayout(self.frame_7)
         self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.label_175 = QLabel(self.frame_7)
-        self.label_175.setObjectName(u"label_175")
-        self.label_175.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.gridLayout_3.addWidget(self.label_175, 0, 0, 1, 1)
-
-        self.pushButton_264 = QPushButton(self.frame_7)
-        self.pushButton_264.setObjectName(u"pushButton_264")
-        self.pushButton_264.setMaximumSize(QSize(30, 30))
-
-        self.gridLayout_3.addWidget(self.pushButton_264, 0, 1, 1, 1)
-
         self.splitter_347 = QSplitter(self.frame_7)
         self.splitter_347.setObjectName(u"splitter_347")
         self.splitter_347.setOrientation(Qt.Orientation.Horizontal)
@@ -71,11 +58,11 @@ class Ui_water_tool(object):
 
         self.gridLayout_3.addWidget(self.splitter_347, 1, 0, 1, 2)
 
-        self.label_176 = QLabel(self.frame_7)
-        self.label_176.setObjectName(u"label_176")
-        self.label_176.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.pushButton_264 = QPushButton(self.frame_7)
+        self.pushButton_264.setObjectName(u"pushButton_264")
+        self.pushButton_264.setMaximumSize(QSize(30, 30))
 
-        self.gridLayout_3.addWidget(self.label_176, 2, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.pushButton_264, 0, 1, 1, 1)
 
         self.splitter_2 = QSplitter(self.frame_7)
         self.splitter_2.setObjectName(u"splitter_2")
@@ -140,104 +127,18 @@ class Ui_water_tool(object):
 
         self.gridLayout_3.addWidget(self.splitter_2, 3, 0, 1, 2)
 
-        self.groupBox_18 = QGroupBox(self.frame_7)
-        self.groupBox_18.setObjectName(u"groupBox_18")
-        self.groupBox_18.setMaximumSize(QSize(270, 16777215))
-        self.gridLayout_2 = QGridLayout(self.groupBox_18)
-        self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.splitter_356 = QSplitter(self.groupBox_18)
-        self.splitter_356.setObjectName(u"splitter_356")
-        self.splitter_356.setOrientation(Qt.Orientation.Horizontal)
-        self.splitter_356.setHandleWidth(0)
-        self.label_178 = QLabel(self.splitter_356)
-        self.label_178.setObjectName(u"label_178")
-        self.splitter_356.addWidget(self.label_178)
-        self.comboBox_5 = QComboBox(self.splitter_356)
-        self.comboBox_5.addItem("")
-        self.comboBox_5.addItem("")
-        self.comboBox_5.addItem("")
-        self.comboBox_5.setObjectName(u"comboBox_5")
-        self.splitter_356.addWidget(self.comboBox_5)
+        self.label_175 = QLabel(self.frame_7)
+        self.label_175.setObjectName(u"label_175")
+        self.label_175.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.gridLayout_2.addWidget(self.splitter_356, 0, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.label_175, 0, 0, 1, 1)
 
-        self.splitter_358 = QSplitter(self.groupBox_18)
-        self.splitter_358.setObjectName(u"splitter_358")
-        self.splitter_358.setOrientation(Qt.Orientation.Horizontal)
-        self.splitter_358.setHandleWidth(0)
-        self.label_180 = QLabel(self.splitter_358)
-        self.label_180.setObjectName(u"label_180")
-        self.splitter_358.addWidget(self.label_180)
-        self.comboBox_10 = QComboBox(self.splitter_358)
-        self.comboBox_10.addItem("")
-        self.comboBox_10.addItem("")
-        self.comboBox_10.addItem("")
-        self.comboBox_10.setObjectName(u"comboBox_10")
-        self.splitter_358.addWidget(self.comboBox_10)
+        self.label_176 = QLabel(self.frame_7)
+        self.label_176.setObjectName(u"label_176")
+        self.label_176.setMaximumSize(QSize(280, 16777215))
+        self.label_176.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.gridLayout_2.addWidget(self.splitter_358, 1, 0, 1, 1)
-
-        self.splitter_360 = QSplitter(self.groupBox_18)
-        self.splitter_360.setObjectName(u"splitter_360")
-        self.splitter_360.setOrientation(Qt.Orientation.Horizontal)
-        self.splitter_360.setHandleWidth(0)
-        self.label_182 = QLabel(self.splitter_360)
-        self.label_182.setObjectName(u"label_182")
-        self.splitter_360.addWidget(self.label_182)
-        self.comboBox_6 = QComboBox(self.splitter_360)
-        self.comboBox_6.addItem("")
-        self.comboBox_6.addItem("")
-        self.comboBox_6.addItem("")
-        self.comboBox_6.setObjectName(u"comboBox_6")
-        self.splitter_360.addWidget(self.comboBox_6)
-
-        self.gridLayout_2.addWidget(self.splitter_360, 2, 0, 1, 1)
-
-        self.splitter_359 = QSplitter(self.groupBox_18)
-        self.splitter_359.setObjectName(u"splitter_359")
-        self.splitter_359.setOrientation(Qt.Orientation.Horizontal)
-        self.splitter_359.setHandleWidth(0)
-        self.label_181 = QLabel(self.splitter_359)
-        self.label_181.setObjectName(u"label_181")
-        self.splitter_359.addWidget(self.label_181)
-        self.comboBox_9 = QComboBox(self.splitter_359)
-        self.comboBox_9.addItem("")
-        self.comboBox_9.addItem("")
-        self.comboBox_9.addItem("")
-        self.comboBox_9.setObjectName(u"comboBox_9")
-        self.splitter_359.addWidget(self.comboBox_9)
-
-        self.gridLayout_2.addWidget(self.splitter_359, 3, 0, 1, 1)
-
-        self.splitter_357 = QSplitter(self.groupBox_18)
-        self.splitter_357.setObjectName(u"splitter_357")
-        self.splitter_357.setOrientation(Qt.Orientation.Horizontal)
-        self.splitter_357.setHandleWidth(0)
-        self.label_179 = QLabel(self.splitter_357)
-        self.label_179.setObjectName(u"label_179")
-        self.splitter_357.addWidget(self.label_179)
-        self.comboBox_8 = QComboBox(self.splitter_357)
-        self.comboBox_8.addItem("")
-        self.comboBox_8.addItem("")
-        self.comboBox_8.addItem("")
-        self.comboBox_8.setObjectName(u"comboBox_8")
-        self.splitter_357.addWidget(self.comboBox_8)
-
-        self.gridLayout_2.addWidget(self.splitter_357, 4, 0, 1, 1)
-
-        self.pushButton_280 = QPushButton(self.groupBox_18)
-        self.pushButton_280.setObjectName(u"pushButton_280")
-
-        self.gridLayout_2.addWidget(self.pushButton_280, 5, 0, 1, 1)
-
-
-        self.gridLayout_3.addWidget(self.groupBox_18, 5, 0, 1, 2)
-
-        self.label_177 = QLabel(self.frame_7)
-        self.label_177.setObjectName(u"label_177")
-        self.label_177.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.gridLayout_3.addWidget(self.label_177, 4, 0, 1, 2)
+        self.gridLayout_3.addWidget(self.label_176, 2, 0, 1, 1)
 
 
         self.gridLayout.addWidget(self.frame_7, 0, 0, 1, 1)
@@ -250,12 +151,10 @@ class Ui_water_tool(object):
 
     def retranslateUi(self, water_tool):
         water_tool.setWindowTitle(QCoreApplication.translate("water_tool", u"Form", None))
-        self.label_175.setText(QCoreApplication.translate("water_tool", u"water", None))
-        self.pushButton_264.setText(QCoreApplication.translate("water_tool", u"X", None))
         self.main_waterfall.setText(QCoreApplication.translate("water_tool", u"waterfall", None))
         self.main_river.setText(QCoreApplication.translate("water_tool", u"river", None))
         self.main_Still_water.setText(QCoreApplication.translate("water_tool", u"Still water", None))
-        self.label_176.setText(QCoreApplication.translate("water_tool", u"sub functions", None))
+        self.pushButton_264.setText(QCoreApplication.translate("water_tool", u"X", None))
         self.sub_waves.setText(QCoreApplication.translate("water_tool", u"waves", None))
         self.sub_ripples.setText(QCoreApplication.translate("water_tool", u"ripples", None))
         self.sub_splashes.setText(QCoreApplication.translate("water_tool", u"splashes", None))
@@ -268,33 +167,7 @@ class Ui_water_tool(object):
         self.sub_wet_edge.setText(QCoreApplication.translate("water_tool", u"wet edge", None))
         self.sub_caustics.setText(QCoreApplication.translate("water_tool", u"caustics ", None))
         self.sub_distortion.setText(QCoreApplication.translate("water_tool", u"distortion", None))
-        self.groupBox_18.setTitle("")
-        self.label_178.setText(QCoreApplication.translate("water_tool", u"intensity ", None))
-        self.comboBox_5.setItemText(0, QCoreApplication.translate("water_tool", u"weak", None))
-        self.comboBox_5.setItemText(1, QCoreApplication.translate("water_tool", u"strong", None))
-        self.comboBox_5.setItemText(2, QCoreApplication.translate("water_tool", u"normal", None))
-
-        self.label_180.setText(QCoreApplication.translate("water_tool", u"power", None))
-        self.comboBox_10.setItemText(0, QCoreApplication.translate("water_tool", u"weak", None))
-        self.comboBox_10.setItemText(1, QCoreApplication.translate("water_tool", u"strong", None))
-        self.comboBox_10.setItemText(2, QCoreApplication.translate("water_tool", u"normal", None))
-
-        self.label_182.setText(QCoreApplication.translate("water_tool", u"opacity", None))
-        self.comboBox_6.setItemText(0, QCoreApplication.translate("water_tool", u"weak", None))
-        self.comboBox_6.setItemText(1, QCoreApplication.translate("water_tool", u"strong", None))
-        self.comboBox_6.setItemText(2, QCoreApplication.translate("water_tool", u"normal", None))
-
-        self.label_181.setText(QCoreApplication.translate("water_tool", u"randomness        ", None))
-        self.comboBox_9.setItemText(0, QCoreApplication.translate("water_tool", u"weak", None))
-        self.comboBox_9.setItemText(1, QCoreApplication.translate("water_tool", u"strong", None))
-        self.comboBox_9.setItemText(2, QCoreApplication.translate("water_tool", u"normal", None))
-
-        self.label_179.setText(QCoreApplication.translate("water_tool", u"grain             ", None))
-        self.comboBox_8.setItemText(0, QCoreApplication.translate("water_tool", u"weak", None))
-        self.comboBox_8.setItemText(1, QCoreApplication.translate("water_tool", u"strong", None))
-        self.comboBox_8.setItemText(2, QCoreApplication.translate("water_tool", u"normal", None))
-
-        self.pushButton_280.setText(QCoreApplication.translate("water_tool", u"color", None))
-        self.label_177.setText(QCoreApplication.translate("water_tool", u"setings", None))
+        self.label_175.setText(QCoreApplication.translate("water_tool", u"water", None))
+        self.label_176.setText(QCoreApplication.translate("water_tool", u"sub functions", None))
     # retranslateUi
 
